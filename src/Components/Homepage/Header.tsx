27b2649118dc5ecbeb9db2b89f4780a8e42527b5
Sayak Saha <Sayak.saha@oracle.com>
2024-03-13 01:42:39 +0530

    updated evrything

---
+++ src/Components/Homepage/Header.tsx
@@ -0,0 +1,9 @@
+import './HeaderStyle.scss'
+
+export const Header = () => {
+  return (
+        <nav className="header-sub">
+            <img src='../../../public/Assests/123_airbnb.jpg' alt='airbnb' className="logo-airbnb"/>
+        </nav>
+  )
+}
