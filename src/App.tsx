83de03a63165e0942fa85e21cdc573af37e2b2de
Sayak Saha <Sayak.saha@oracle.com>
2024-03-13 23:32:28 +0530

    added body

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Header } from './Components/Homepage/Header'
+import { Body } from './Components/Body(Homepage)/Body'
+import { Header } from './Components/Header(Homepage)/Header'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
     <Header/>
+    <Body/>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/Components/Body(Homepage)/Body.tsx
@@ -0,0 +1,9 @@
+import '../Body(Homepage)/BodyStyle.scss'
+
+export const Body =()=>{
+    return(
+        <div className="gird-view">
+            <img className="grid-image"src='../../../public/Assests/grid.png' alt="images" />
+        </div>
+    )
+}
